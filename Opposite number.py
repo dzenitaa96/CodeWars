ccb77962e8ece8aaf53de1b80e6a8bf55708762b
Dženita <80351327+dzenitaa96@users.noreply.github.com>
2021-06-16 22:26:56 +0200

    Add files via upload

---
+++ Opposite number.py
@@ -0,0 +1,11 @@
+# Very simple, given a number, find its opposite.
+
+# Accepted solution
+def opposite(number):
+    number = -number
+    return number
+
+# Another solution
+def opposite(number):
+    return -number
+
